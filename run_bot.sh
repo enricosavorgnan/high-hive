68fb3233a9935e79084bb8c23bdd7c01ff5d9764
enricosavorgnan <enricosavorgnan@gmail.com>
2026-04-14 22:48:40 +0200

    code: improve run_bot.sh

--- run_bot.sh
+++ run_bot.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "$0")" && pwd)"
-exec "$DIR/cpp/build/uhp --engine AlphaZeroEngine --model-path $DIR/cpp/src/alphaZeroEngine/checkpoints/pretrained_best.pt --time-budget 4500"
+
+# exec need to follow the structure:
+# exec <path_to_executable> --engine <engine_name> --model-path <path_to_model> --time-budget <time_in_ms>
+# --model-path is required for --engine AlphaZeroEngine
+# --time-budget is required for --engine AlphaZeroEngine, and measures milliseconds
+exec "$DIR/cpp/build/uhp" --engine AlphaZeroEngine --model-path "$DIR/cpp/src/alphaZeroEngine/checkpoints/pretrained_best.pt" --time-budget 4500
